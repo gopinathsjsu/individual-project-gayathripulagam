ced89b2c0c5576405af2445b5e52f747f07c42be
Gayathri Pulagam <gai3.pulagam@gmail.com>
2021-04-30 22:02:17 -0700

    dummy test

---
+++ src/test/java/com/gayathri/CreditCardTest.java
@@ -0,0 +1,13 @@
+package com.gayathri;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CreditCardTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
